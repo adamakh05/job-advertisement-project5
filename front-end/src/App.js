70ea9a651a4ceb2c1c1cd88c0db8c071ef3131c1
adamakh05 <aoo4mmm@gmail.com>
2025-03-04 14:25:54 +0000

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,3 +1,21 @@
 // javascript code for app.js
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
