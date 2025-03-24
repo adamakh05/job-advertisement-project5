87bd1544e5cdd3be3917252554bb12a816e00e30
adamakh05 <aoo4mmm@gmail.com>
2025-03-24 13:09:03 +0000

    Update index.js

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,1 @@
-import React from 'react'; // imports react elements
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root')); //creates react element
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-reportWebVitals();
