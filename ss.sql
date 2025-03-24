3c3f70de98003931a3ccc9369a86637cfa4d8a8f
shabbir2005 <shabbir.ahmad78631@gmail.com>
2025-03-24 13:00:11 +0000

    Update ss.sql

--- ss.sql
+++ ss.sql
@@ -15,32 +15,7 @@ INSERT INTO users (id, email, password, username, dob, role) VALUES
   (5, 'admin@example.com', '$2b$10$xw73tzF0xC2MvVf9IEsbTOAgj2AoC5YgSq9enKIOHiuMPv.dfcxZW', 'admin', '1990-01-01', 'admin');
 
 
--- Create `applications` table
-CREATE TABLE applications (
-  id INT NOT NULL AUTO_INCREMENT,
-  job_id INT NOT NULL,
-  user_id INT NOT NULL,
-  name VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  cover_letter TEXT,
-  status ENUM('PENDING', 'REVIEWED', 'SHORTLISTED', 'REJECTED') DEFAULT 'PENDING',
-  PRIMARY KEY (id)
-);
 
--- Populate `applications` table
-INSERT INTO applications (id, job_id, user_id, name, email, cover_letter, status) VALUES
-  (3, 2, 3, 'sss', 'sss@ss.com', 'ssss', 'PENDING'),
-  (4, 2, 3, 'sss', 'sss@ss.com', 'sssss', 'PENDING'),
-  (5, 1, 3, 'sss', 'sss@ss.com', 'sadadadadsa', 'PENDING'),
-  (6, 1, 3, 'sss', 'sss@ss.com', 'ssss', 'PENDING'),
-  (7, 1, 3, 'sss', 'sss@ss.com', 'ss', 'PENDING'),
-  (8, 2, 3, 'ss', 'sss@ss.com', 'ssssssss', 'PENDING'),
-  (10, 1, 3, 'ss', 'sss@ss.com', 'sss', 'PENDING'),
-  (12, 1, 3, 'sss', 'sss@ss.com', 'sss', 'PENDING'),
-  (13, 1, 3, 'sss', 'sss@ss.com', 'sss', 'PENDING'),
-  (14, 1, 3, 'ssss', 'sss@ss.com', 'ssss', 'PENDING'),
-  (15, 7, 3, 'ssss', 'sss@ss.com', 'ssss', 'PENDING'),
-  (16, 7, 3, 'sssss', 'sss@ss.com', 'ssss', 'PENDING');
 
 -- Create `jobs` table
 CREATE TABLE jobs (
