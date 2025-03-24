200df106a442789c6c8e12139f23a0df97cff842
Haris863 <haris.mahmoodpro863@gmail.com>
2025-03-24 13:36:20 +0000

    Update performance-helpers.js

--- job-management-backend/performance-helpers.js
+++ job-management-backend/performance-helpers.js
@@ -1,1 +1,20 @@
+const { faker } = require('@faker-js/faker');
+
+function generateUserCredentials(userContext, events, done) {
+  // Generate random user data
+  const username = faker.internet.userName();
+  const email = faker.internet.email();
+  const password = faker.internet.password(10);
+  
+  // Add to context
+  userContext.vars.username = username;
+  userContext.vars.email = email;
+  userContext.vars.password = password;
+  
+  return done();
+}
+
+module.exports = {
+  generateUserCredentials
+};
 
