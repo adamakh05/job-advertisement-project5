aeec213d93f0a8aa85cda24554a3f2a80d102725
Ahmed2599 <aali290@bradford.ac.uk>
2025-03-25 14:39:43 +0000

    Update api.test.js

--- job-management-backend/api.test.js
+++ job-management-backend/api.test.js
@@ -1,1 +1,26 @@
+const request = require('supertest');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const { app } = require('./server');
 
+
+jest.mock('mysql2/promise', () => {
+  const mockExecute = jest.fn();
+  const mockConnection = {
+    execute: mockExecute,
+    release: jest.fn(),
+    beginTransaction: jest.fn(),
+    commit: jest.fn(),
+    rollback: jest.fn()
+  };
+  return {
+    createPool: jest.fn().mockReturnValue({
+      getConnection: jest.fn().mockResolvedValue(mockConnection),
+      execute: mockExecute
+    })
+  };
+});
+
+
+const mysql = require('mysql2/promise');
+const mockPool = mysql.createPool();
